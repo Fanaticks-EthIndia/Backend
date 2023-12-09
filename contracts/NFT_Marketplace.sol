a99a07a87dc9b817160caf383d107745bcbc77c7
Mihir Kuma Singh <mihir39nr@gmail.com>
2023-12-09 12:58:34 +0530

    Improved Smart Contract

--- contracts/NFT_Marketplace.sol
+++ contracts/NFT_Marketplace.sol
@@ -1,55 +1,76 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
-contract Leaderboard {
-    using SafeMath for uint256;
-    using Counters for Counters.Counter;
-
-    bool ready;
-    mapping(address => uint256) public poi;
-    address public owner;
-    Counters.Counter private _tokenIds;
-    address contractAddress;
-    event ScoreUpdated(address indexed player, uint256 newScore);
-
-    constructor(address marketplaceAddress) {
-        ready = false;
-        owner = msg.sender;
-        contractAddress = marketplaceAddress;
+contract MyToken is ERC1155, Ownable, ERC1155Supply {
+    struct Event {
+        uint256 timestamp;
+        address[] attendees ;
+    }
+    struct Artist { 
+        mapping(address => uint256) poi;
+        address artist_address;
+        address[] poiList;
+        uint64 price;
+        Event[] events; 
     }
 
-    event ScoreUpdated(address indexed player, uint256 newScore);
+    mapping(string => Artist) catalogue;
+    address contractAddress;
+    event POIUpdated(address indexed player, uint256 newScore);
+    constructor(address initialOwner) ERC1155("") Ownable(initialOwner)
+    {
 
-    function incrementScore(uint256 _incrementBy,address _sender) external {
-        if(poi.containsKey(_sender))
+    }
+
+    function incrementScore(uint256 _incrementBy,address _sender,string artist) external {
+        if(catalogue['artist'].poiList[_sender])
         {
-            poi[_sender] = 1;
+            catalogue['artist'].poiList.push(_sender);
         }
-        else
-        {
-            poi[_sender] += _incrementBy;
-        }
+        poi[_sender] += _incrementBy;
 
         emit ScoreUpdated(_sender, poi[_sender]);
     }
 
-    function createToken(string memory tokenURI) public returns (uint) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        setApprovalForAll(contractAddress, true);
-        return newItemId;
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
     }
 
-    function getLeaderboard() external view returns (address[] memory) {
-        return leaderboard;
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256[] memory ids, uint256[] memory values)
+        internal
+        override(ERC1155, ERC1155Supply)
+    {
+        super._update(from, to, ids, values);
+    }
+
+
+    // To be completed
+    function sale(uint256 seats, uint seats_poi,int price,string artist) public
+    {   
+        if(catalogue['artist'].poiList.length < seats_poi)
+        {
+            mintBatch(to, ids, amounts, data); 
+        }
+        
     }
 }
